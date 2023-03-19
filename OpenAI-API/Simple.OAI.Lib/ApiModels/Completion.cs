2df685674d139e1f82a3e83a6d4f0f73740583da
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-03-19 14:36:12 -0300

    Api Models

---
+++ OpenAI-API/Simple.OAI.Lib/ApiModels/Completion.cs
@@ -0,0 +1,34 @@
+﻿namespace Simple.OAI.Lib.ApiModels;
+
+public class CompletionRequest
+{
+    public string Model { get; set; }
+    public string Prompt { get; set; }
+    public int? Max_Tokens { get; set; }
+    public double Temperature { get; set; }
+
+}
+public class CompletionResponse
+{ 
+    public string id { get; set; }
+    public string _object { get; set; }
+    public int created { get; set; }
+    public string model { get; set; }
+    public Choice[] choices { get; set; }
+    public Usage usage { get; set; }
+}
+
+public class Usage
+{
+    public int prompt_tokens { get; set; }
+    public int completion_tokens { get; set; }
+    public int total_tokens { get; set; }
+}
+
+public class Choice
+{
+    public string text { get; set; }
+    public int index { get; set; }
+    public int? logprobs { get; set; }
+    public string finish_reason { get; set; }
+}
---
+++ OpenAI-API/Simple.OAI.Lib/ApiModels/ListModels.cs
@@ -0,0 +1,40 @@
+﻿namespace Simple.OAI.Lib.ApiModels;
+
+internal class ListModels
+{
+    public string _object { get; set; }
+    public Model[] data { get; set; }
+}
+
+public class Model
+{
+    public string id { get; set; }
+    public string _object { get; set; }
+    public int created { get; set; }
+    public string owned_by { get; set; }
+    public Permission[] permission { get; set; }
+    public string root { get; set; }
+    public object parent { get; set; }
+
+    public override string ToString()
+    {
+        return id;
+    }
+}
+
+public class Permission
+{
+    public string id { get; set; }
+    public string _object { get; set; }
+    public int created { get; set; }
+    public bool allow_create_engine { get; set; }
+    public bool allow_sampling { get; set; }
+    public bool allow_logprobs { get; set; }
+    public bool allow_search_indices { get; set; }
+    public bool allow_view { get; set; }
+    public bool allow_fine_tuning { get; set; }
+    public string organization { get; set; }
+    public object group { get; set; }
+    public bool is_blocking { get; set; }
+}
+
