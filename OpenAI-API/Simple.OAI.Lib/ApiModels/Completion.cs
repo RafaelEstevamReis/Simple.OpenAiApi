f813dd84b2e1612e03ffe6dd7c60516fb0952fe8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-03-19 15:43:07 -0300

    Move usage

--- OpenAI-API/Simple.OAI.Lib/ApiModels/Completion.cs
+++ OpenAI-API/Simple.OAI.Lib/ApiModels/Completion.cs
@@ -9,20 +9,13 @@ public class CompletionRequest
 
 }
 public class CompletionResponse
-{ 
+{
     public string id { get; set; }
     public string _object { get; set; }
     public int created { get; set; }
     public string model { get; set; }
     public Choice[] choices { get; set; }
     public Usage usage { get; set; }
-}
-
-public class Usage
-{
-    public int prompt_tokens { get; set; }
-    public int completion_tokens { get; set; }
-    public int total_tokens { get; set; }
 }
 
 public class Choice
---
+++ OpenAI-API/Simple.OAI.Lib/ApiModels/Usage.cs
@@ -0,0 +1,15 @@
+﻿namespace Simple.OAI.Lib.ApiModels;
+
+public class Usage
+{
+    public int prompt_tokens { get; set; }
+    public int completion_tokens { get; set; }
+    public int total_tokens { get; set; }
+
+    public static Usage operator +(Usage a, Usage b) => new Usage()
+    {
+        completion_tokens = a.completion_tokens + b.completion_tokens,
+        prompt_tokens = a.prompt_tokens + b.prompt_tokens,
+        total_tokens = a.total_tokens + b.total_tokens,
+    };
+}
