35a734994008a908c3339294783e2f97cc847423
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-03-26 20:56:26 -0300

    Add summary

--- Simple.OAI.Lib/OpenAI.cs
+++ Simple.OAI.Lib/OpenAI.cs
@@ -2,11 +2,16 @@
 using System.Threading.Tasks;
 
 namespace Simple.OpenAI;
-
+/// <summary>
+/// Requests data from OpenAI using it's API
+/// </summary>
 public class OpenAI
 {
     private readonly ClientInfo client;
-
+    /// <summary>
+    /// Creates an OpenAPI API object using user's API Key
+    /// </summary>
+    /// <param name="key">User's API Key</param>
     public OpenAI(string key)
     {
         client = new ClientInfo("https://api.openai.com/");
@@ -16,9 +21,13 @@ public class OpenAI
 
     private void Client_ResponseDataReceived(object sender, ClientInfo.ResponseReceived e)
     {
-        
+        // Debug
     }
 
+    /// <summary>
+    /// Lists the currently available models
+    /// </summary>
+    /// <returns>Models information</returns>
     public async Task<ApiModels.Model[]> GetModelsAsync()
     {
         // GET https://api.openai.com/v1/models
@@ -26,6 +35,11 @@ public class OpenAI
         resp.EnsureSuccessStatusCode();
         return resp.Data.data;
     }
+    /// <summary>
+    /// Provides basic information about each model such as the owner and availability.
+    /// </summary>
+    /// <param name="modelId">Model id</param>
+    /// <returns>Model information</returns>
     public async Task<ApiModels.Model> GetModelAsync(string modelId)
     {
         // GET https://api.openai.com/v1/models
@@ -34,6 +48,11 @@ public class OpenAI
         return resp.Data;
     }
 
+    /// <summary>
+    /// Creates a completion for the provided prompt and parameters
+    /// </summary>
+    /// <param name="request">Request parameters</param>
+    /// <returns>Chat response</returns>
     public async Task<ApiModels.CompletionResponse> TextCompletion(ApiModels.CompletionRequest request)
     {
         var resp = await client.PostAsync<ApiModels.CompletionResponse>("v1/completions", request);
@@ -42,6 +61,11 @@ public class OpenAI
         return resp.Data;
     }
 
+    /// <summary>
+    /// Creates a completion for the chat message
+    /// </summary>
+    /// <param name="request">Request parameters</param>
+    /// <returns>Chat response</returns>
     public async Task<ApiModels.ChatResponse> ChatCompletionAsync(ApiModels.ChatRequest request)
     {
         var resp = await client.PostAsync<ApiModels.ChatResponse>("v1/chat/completions", request);
